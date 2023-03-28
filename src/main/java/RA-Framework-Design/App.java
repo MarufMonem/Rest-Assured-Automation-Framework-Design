ab75e64f8d75699afbb11ef2ccdda772311b8d8d
Maruf_Monem <marufmonem@gmail.com>
2023-03-29 04:58:33 +0600

    Maven project created

---
+++ src/main/java/RA-Framework-Design/App.java
@@ -0,0 +1,13 @@
+package RA-Framework-Design;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
