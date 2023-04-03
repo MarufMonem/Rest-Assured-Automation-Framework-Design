a59f6434ac5cc268c185ea9b2dc5e1081fe30f95
Maruf_Monem <marufmonem@gmail.com>
2023-04-04 05:16:34 +0600

    Logging functionality added

--- src/test/java/Resources/Utils.java
+++ src/test/java/Resources/Utils.java
@@ -1,14 +1,23 @@
 package Resources;
 
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 
 public class Utils {
     RequestSpecification req;
 
-    public RequestSpecification requestSpecifications(){
+    public RequestSpecification requestSpecifications() throws FileNotFoundException {
+        PrintStream log = new PrintStream(new FileOutputStream("logging.txt"));
         req = new RequestSpecBuilder().setContentType(ContentType.JSON)
+                .addFilter(RequestLoggingFilter.logRequestTo(log))
+                .addFilter(ResponseLoggingFilter.logResponseTo(log))
                 .setBaseUri("https://rahulshettyacademy.com")
                 .addQueryParam("key", "qaclick123")
                 .setContentType(ContentType.JSON)
--- src/test/java/stepDefinitions/StepDefs.java
+++ src/test/java/stepDefinitions/StepDefs.java
@@ -1,15 +1,12 @@
 package stepDefinitions;
 
 import POJO.AddPlace;
-import POJO.Location;
 import Resources.TestDataBuild;
 import Resources.Utils;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
@@ -18,8 +15,7 @@ import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 import org.junit.Assert;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.FileNotFoundException;
 
 import static io.restassured.RestAssured.given;
 
@@ -32,15 +28,16 @@ public class StepDefs extends Utils{
     String responseBody;
     JsonPath js;
     @Given("Add place payload")
-    public void addPlacePayload() {
+    public void addPlacePayload() throws FileNotFoundException {
+        //payload
         TestDataBuild td = new TestDataBuild();
         ap=td.addPlacePayLoad();
-
+        // Request specification
         req = requestSpecifications();
 
-        resSpec = new ResponseSpecBuilder()
-                .expectStatusCode(200)
-                .expectContentType(ContentType.JSON).build();
+//        resSpec = new ResponseSpecBuilder()
+//                .expectStatusCode(200)
+//                .expectContentType(ContentType.JSON).build();
 
         res = given().spec(req).body(ap);
 
@@ -51,7 +48,7 @@ public class StepDefs extends Utils{
     public void userCallsWithPostHttpRequest(String arg0) {
         responseJson = res.when()
                 .post("/maps/api/place/add/json")
-                .then().spec(resSpec).extract().response();
+                .then().extract().response();
     }
 
     @Then("The API call will get success with status code {int}")
